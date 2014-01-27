f09c1b30dd1d43af63da64969e7612e5bd687883
Andy Duncan <andy@duncaningram.com>
2014-01-27 00:45:05 -0500

    Working simulation with default 20 nodes and one transmission (ftp).

--- simulation/manet.tcl
+++ simulation/manet.tcl
@@ -4,6 +4,8 @@
 # Lifted and expanded from:
 # https://code.google.com/p/ns-allinone-2-34-imp-protocol/source/browse/trunk/src/ns-2.34/aodv/example.tcl?r=11
 
+# Todo: Review documentation further: http://www.isi.edu/nsnam/ns/doc/ns_doc.pdf
+
 # Todo: Allow command line processing of some setup parameters, including number of nodes.
 
 proc setup {} {
@@ -16,7 +18,7 @@ proc setup {} {
 	set val(ll)             LL                         ;# link layer type
 	set val(ant)            Antenna/OmniAntenna        ;# antenna model
 	set val(ifqlen)         50                         ;# max packet in ifq
-	set val(nn)             10                         ;# number of mobilenodes
+	set val(nn)             20                         ;# number of mobilenodes
 	set val(rp)             AODV                       ;# routing protocol
 	set val(x)              500                        ;# X dimension of topography
 	set val(y)              400                        ;# Y dimension of topography
@@ -52,8 +54,15 @@ foreach key [array names val] {
     puts "${key}:\t$val($key)"
 }
 
-# exit 0
+# Todo accept seed in setup.
+global defaultRNG
+$defaultRNG seed 99999
 
+set topology_ [new RandomVariable/Uniform]
+$topology_ set min_ $val(x)
+$topology_ set max_ $val(y)
+
+puts [format "Got random topology value = %d" [expr round([$topology_ value])]]
 
 set ns            [new Simulator]
 set tracefd       [open manet.tr w]
@@ -92,56 +101,33 @@ $ns node-config -adhocRouting $val(rp) \
 
 for {set i 0} {$i < $val(nn) } { incr i } {
     set node_($i) [$ns node]
-}
+    # Create random movement for nodes
+    $node_($i) random-motion 1
 
-# Todo: Provide initial location across topology for N mobile nodes.
-# Todo: Determine or define the value representation of X/Y, assume meters?
-
-for {set i 0} {$i < $val(nn) } { incr i } {
-	$node_($i) set X_ 5.0 + $i
-	$node_($i) set Y_ 5.0 + $i
+	# Provide initial location across topology for N mobile nodes.
+	$node_($i) set X_ $topology_ value
+	$node_($i) set Y_ $topology_ value
 	$node_($i) set Z_ 0.0
+
+	# Begin random movement from start distance at time zero.
+	$ns at 1.0 "$node_($i) start"
+
 }
 
+# Todo: Simulate specific movement across topology for N mobile nodes.
+# for {set i 0} {$i < $val(nn) } { incr i } {
+	# puts [format "$node_($i) at 10.0 setdest %d %d 10.0" [expr round([$topology_ value])] [expr round([$topology_ value])] ]
+	# $ns at 10.0 [format "$node_($i) setdest 400.0 400.0 10.0" [expr $i]]
+# }
 
-# Provide initial location of mobilenodes
-# $node_(0) set X_ 5.0
-# $node_(0) set Y_ 5.0
-# $node_(0) set Z_ 0.0
 
-# $node_(1) set X_ 280
-# $node_(1) set Y_ 180.0
-# $node_(1) set Z_ 0.0
-
-#$node_(2) set X_ 150.0
-#$node_(2) set Y_ 240.0
-#$node_(2) set Z_ 0.0
-
-#$node_(3) set X_ 250.0
-#$node_(3) set Y_ 240.0
-#$node_(3) set Z_ 0.0
-
-#$node_(4) set X_ 100.0
-#$node_(4) set Y_ 70.0
-#$node_(4) set Z_ 0.0
-
-# Todo: Simulate random movement across topology for N mobile nodes.
-
-# Generation of movements
-$ns at 10.0 "$node_(0) setdest 250.0 250.0 10.0"
-$ns at 15.0 "$node_(1) setdest 200.0 300.0 10.0"
-#$ns at 110.0 "$node_(0) setdest 480.0 300.0 10.0"
-#$ns at 70.0 "$node_(3) setdest 300.0 320.0 10.0"
-#$ns at 80.0 "$node_(4) setdest 180.0 280.0 10.0"
-#$ns at 20.0 "$node_(2) setdest 10.0 280.0 10.0"
-
-#---------------------- configure node 0 and node 1
-#---------------------- node 0 as tcp and node 1 as sink
+# configure node 0 and node 1
+# node 0 as tcp and node 1 as sink
 set tcp01 [new Agent/TCP/Newreno]
 $ns attach-agent $node_(0) $tcp01
 
 set sink01 [new Agent/TCPSink]
-$ns attach-agent $node_(1) $sink01
+$ns attach-agent $node_([expr $val(nn) - 1 ]) $sink01
 
 $ns connect $tcp01 $sink01
 
